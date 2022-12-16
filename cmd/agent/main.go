9287452c54f1ad5f5741607dd2bf6892c6c2ecbe
otvet8 <arsabramoff@gmail.com>
2022-12-16 20:41:33 +0300

    Inc1

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"time"
+
+	"github.com/AbramovArseniy/RuntimeMetrics/tree/main/internal/agent"
+)
+
+const (
+	pollRuntimeMetricsInterval = 2 * time.Second
+	reportInterval             = 10 * time.Second
+)
+
+func main() {
+	go agent.Schedule(agent.CollectRuntimeMetrics, pollRuntimeMetricsInterval)
+	go agent.Schedule(agent.SendMetrics, reportInterval)
+}
