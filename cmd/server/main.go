e98c9e761b9c81e5be1bd865759816bcf52deae9
otvet8 <arsabramoff@gmail.com>
2022-12-16 20:48:57 +0300

    inc1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,27 @@
 package main
 
-func main() {}
+import (
+	"log"
+	http "net/http"
+)
+
+const (
+	Server = "localhost"
+	Port   = "8080"
+)
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Hello, World</h1>"))
+}
+
+func StartServer() {
+	srv := &http.Server{
+		Addr: Server + ":" + Port,
+	}
+	http.HandleFunc("/", HelloWorld)
+	log.Fatal(srv.ListenAndServe())
+}
+
+func main() {
+	StartServer()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AbramovArseniy/RuntimeMetrics
+
+go 1.18
