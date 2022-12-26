cd02c0104f068935e6534530d03d0d6137679130
otvet8 <arsabramoff@gmail.com>
2022-12-26 18:54:58 +0300

    inc3: Added Get Handlers and chi Router

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -5,12 +5,24 @@ import (
 	"log"
 	"net/http"
 	"strconv"
-	"strings"
+
+	"github.com/go-chi/chi/v5"
 )
 
+func GetAllMetricsHandler(rw http.ResponseWriter, _ *http.Request) {
+	log.Println("Get all request")
+	for metricName, metricVal := range Storage.GaugeMetrics {
+		rw.Write([]byte(fmt.Sprintf("%s: %f", metricName, metricVal)))
+	}
+	for metricName, metricVal := range Storage.CounterMetrics {
+		rw.Write([]byte(fmt.Sprintf("%s: %d", metricName, metricVal)))
+	}
+	rw.WriteHeader(http.StatusOK)
+	rw.Header().Add("Content-Type", "text/plain")
+}
+
 func PostMetricHandler(rw http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	metricType, metricName, metricValue := strings.Split(fmt.Sprintf("%v", r.URL), "/")[2], strings.Split(fmt.Sprintf("%v", r.URL), "/")[3], strings.Split(fmt.Sprintf("%v", r.URL), "/")[4]
+	metricType, metricName, metricValue := chi.URLParam(r, "type"), chi.URLParam(r, "name"), chi.URLParam(r, "value")
 	log.Println(metricType, metricName, metricValue)
 	switch metricType {
 	case "gauge":
@@ -30,6 +42,44 @@ func PostMetricHandler(rw http.ResponseWriter, r *http.Request) {
 		log.Printf("%s: %d", metricName, newVal)
 		Storage.CounterMetrics[metricName] += newVal
 	}
-	log.Println(Storage)
+	rw.Header().Add("Content-Type", "text/plain")
+	rw.WriteHeader(http.StatusOK)
 
+}
+
+func GetMetricHandler(rw http.ResponseWriter, r *http.Request) {
+	metricType, metricName := chi.URLParam(r, "type"), chi.URLParam(r, "name")
+	log.Println(metricType, metricName)
+	switch metricType {
+	case "gauge":
+		log.Printf("GetRequest: value of %s", metricName)
+		if metricVal, isIn := Storage.GaugeMetrics[metricName]; isIn {
+			rw.WriteHeader(http.StatusOK)
+			rw.Header().Add("Content-Type", "text/plain")
+			_, err := rw.Write([]byte(fmt.Sprintf("%s: %f", metricName, metricVal)))
+			if err != nil {
+				log.Println(err)
+				return
+			}
+		} else {
+			http.Error(rw, "There is no metric you requested", http.StatusNotFound)
+		}
+
+	case "counter":
+		log.Printf("GetRequest: value of %s", metricName)
+		if metricVal, isIn := Storage.CounterMetrics[metricName]; isIn {
+			rw.WriteHeader(http.StatusOK)
+			rw.Header().Add("Content-Type", "text/plain")
+			_, err := rw.Write([]byte(fmt.Sprintf("%s: %d", metricName, metricVal)))
+			if err != nil {
+				log.Println(err)
+				return
+			}
+		} else {
+			http.Error(rw, "There is no metric you requested", http.StatusNotFound)
+		}
+	default:
+		http.Error(rw, "There is no metric you requested", http.StatusNotFound)
+	}
+	log.Println(Storage)
 }
